bf44ca553959d2494439138ec072b112e44c5cee
Lizardo <lizardo@utp.edu.co>
2021-03-30 13:37:22 -0500

    Se agrega codificacion de mensajes y ajuste a python3
    Signed-off-by: Lizardo <lizardo@utp.edu.co>

--- servidores/socket_server1.py
+++ servidores/socket_server1.py
@@ -11,13 +11,14 @@ mi_socket.listen(1)
 conexion, addr = mi_socket.accept()
 print('Nueva conexion establecida')
 print (addr)
-
-conexion.send("Digame su nombre")
+solinombre = "Digame su nombre"
+conexion.send(solinombre.encode("ascii"))
 peticion = conexion.recv(1024)
+decodpeticion = peticion.decode("ascii")
 hora = time.strftime("%X")
 fecha = time.strftime("%x")
-envio = 'Hola '+ str(peticion)+' usted ha ingresado el '+str(fecha)+' a las '+str(hora)
-conexion.send(envio)
+envio = 'Hola '+ str(decodpeticion)+' usted ha ingresado el '+str(fecha)+' a las '+str(hora)
+conexion.send(envio.encode("ascii"))
 
 conexion.close()
 mi_socket.close()
